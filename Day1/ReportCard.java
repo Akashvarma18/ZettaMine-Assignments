514719ae8c124be3249151919fb4122338cbce71
Akashvarma18 <82646836+Akashvarma18@users.noreply.github.com>
2023-12-11 17:00:09 +0530

    Add files via upload

---
+++ Day1/ReportCard.java
@@ -0,0 +1,54 @@
+package com.nums;
+
+import java.util.Scanner;
+
+public class ReportCard {
+	
+	
+	public static int marksEntry(int num) {
+		Scanner input = new Scanner(System.in);
+		int[] subjectsArray = new int[num];
+		int total = 0;
+		System.out.println("SUBJECT" + " ".repeat(20) + "Marks");
+		for(int i=0; i<num; i++) {
+			System.out.print("Enter Subject " + i+1 + " ".repeat(14));
+			subjectsArray[i] = input.nextInt();		
+		}	
+		
+		for(int i=0; i<subjectsArray.length; i++) {
+			total += subjectsArray[i];
+		}		
+		return total;	
+			
+	}
+	
+	public static void PrintReportCard() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter Student Name: ");
+		String studentName = input.next();
+		System.out.print("Enter no of subjects: ");
+		int subjects = input.nextInt();	
+		int total = marksEntry(subjects);
+		float Average = ((float)total/subjects);
+		System.out.print("Your Average is: ");
+		System.out.printf("% .2f",Average);
+		
+		
+					
+	}
+	
+	
+	
+		
+		
+		
+		
+	
+
+	public static void main(String[] args) {
+		PrintReportCard();
+		
+
+	}
+
+}
