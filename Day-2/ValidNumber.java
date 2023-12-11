9b6af4370ae93da28e822f42b4a89d2e62aa7b5c
Akashvarma18 <82646836+Akashvarma18@users.noreply.github.com>
2023-12-11 16:51:51 +0530

    Add files via upload

---
+++ Day-2/ValidNumber.java
@@ -0,0 +1,23 @@
+package com.Day2;
+
+import java.util.Scanner;
+public class ValidNumber {	
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		System.out.print("Enter your phone number: ");
+		String number = sc.next();
+		Boolean first = number.matches("^[6789][0-9]{9}$");
+		if(first) {
+			System.out.println("True");
+		}else {
+			System.out.println("Insert a relevent number");
+		}
+		
+		
+		
+		
+		
+
+	}
+
+}
